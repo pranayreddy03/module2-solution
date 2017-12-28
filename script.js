9b427e1e12b7ce0e5bd957c596f59d4bad82c32c
pranay <reddy.pranay03@gmail.com>
2017-12-28 10:39:53 -0600

    Final Commit

--- script.js
+++ script.js
@@ -10,23 +10,46 @@
     function ToBuyController(ShoppingListCheckOffService)
     {
         var buy = this;
+        buy.getItems = ShoppingListCheckOffService.getItems();
+        buy.boughtButton = function(index)
+        {
+            ShoppingListCheckOffService.boughtButton(index);
+        }
     }
 
     AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
     function AlreadyBoughtController(ShoppingListCheckOffService)
     {
         var bought = this;
+        bought.getBoughtItems = ShoppingListCheckOffService.getBoughtItems();
     }
 
     function ShoppingListCheckOffService()
     {
         var service = this;
 
-        var items = [
-            {name: "cookies", quantity: 10},{name: "eggs", quantity: 6},{name: "candies", quantity: 23}
-        ];
+        var toBuyItems = [ {name: "cookies", quantity: 6},
+                           {name: "eggs", quantity: 63},
+                           {name: "cakes", quantity: 3},
+                           {name: "candies", quantity: 12},
+                           {name: "donuts", quantity: 5}];
 
+        var boughtItems = [];
 
+        service.getItems = function()
+        {
+            return toBuyItems;
+        };
+
+        service.getBoughtItems = function () {
+            return boughtItems;
+        };
+
+        service.boughtButton = function(itemIndex)
+        {
+            var item = toBuyItems[itemIndex];
+            boughtItems.push(item);
+            toBuyItems.splice(itemIndex,1);
+        }
     }
-
 })();
